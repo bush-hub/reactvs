4302d2a19e94ccb57cd771d7f83b32354a9e29f2
Guest <guest-kqrv2q@h25717.sgb.skir.kr>
2020-01-04 23:07:21 +0900

    new file

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+'use strict'
+
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+ReactDOM.render(
+	<div>
+		Hello world!
+	</div>,
+	document.getElementById('root')
+)
--- tools/base.js
+++ tools/base.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
   entry: {
@@ -42,5 +43,10 @@ module.exports = {
     ],
   },
   // plugins
-  plugins: [],
+  plugins: [
+		new HtmlWebpackPlugin({
+			template: './src/index.html',
+			filename: './index.html',
+		}),
+	]
 };
