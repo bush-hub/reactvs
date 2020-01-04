5687e07c04aa5311457a073a72d64d3572104be2
Guest <guest-kqrv2q@h25717.sgb.skir.kr>
2020-01-04 23:45:12 +0900

    new file

--- tools/production.js
+++ tools/production.js
@@ -1,6 +1,6 @@
 const merge = require('webpack-merge');
-const config = require('./base.config');
+const config = require('./base');
 
 module.exports = merge(config, {
-  mode: 'production',
+  mode: 'production'
 });
